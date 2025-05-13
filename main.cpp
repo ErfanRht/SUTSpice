0506e2870e86f602063cd0f778eced5736c75418
Erfan Rahmati <ErfanRht2005@gmail.com>
2025-05-13 18:00:00 +0330

    feat: Implement Resistor and abstract Component stamping

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <algorithm>
 #include <cctype>
 #include <stdexcept>
+#include <map>
+#include <iomanip>
+#include <sstream>
 using namespace std;
 
 inline string to_lower_util(string s) {
@@ -64,6 +67,9 @@ double parse_value_with_metric_prefix_util(const string& val_str_orig) {
     return base_val * multiplier;
 }
 
+
+class Circuit;
+
 class Component {
 public:
     string name;
@@ -79,24 +85,68 @@ public:
     }
 
     virtual ~Component() = default;
+    virtual void stamp(vector<vector<double>>& G, Circuit& circuit) = 0;
+    virtual string to_netlist_string() const = 0;
 };
 
 class Circuit {
 public:
     vector<unique_ptr<Component>> components;
+    map<string, int> node_to_idx;
+    vector<string> idx_to_node_name;
+    bool ground_node_exists = false;
+    string ground_node_explicit_name = "0";
 
     void add_component(unique_ptr<Component> comp) {
         components.push_back(move(comp));
     }
+
+    bool is_ground(const string& node_name) const {
+        return node_name == ground_node_explicit_name;
+    }
+
+    int get_node_matrix_index(const string& node_name) {
+        if (is_ground(node_name)) return -1;
+        if (node_to_idx.find(node_name) == node_to_idx.end()) {
+            int new_idx = node_to_idx.size();
+            node_to_idx[node_name] = new_idx;
+            idx_to_node_name.push_back(node_name);
+        }
+        return node_to_idx.at(node_name);
+    }
+};
+
+class Resistor : public Component {
+public:
+    Resistor(const string& r_name, const string& n1, const string& n2, const string& val_str)
+            : Component(r_name, n1, n2, val_str) {
+        if (value <= 0) throw runtime_error("Resistor " + name + " must have positive resistance.");
+    }
+
+    string to_netlist_string() const override {
+        ostringstream oss;
+        oss << name << " " << node1_name << " " << node2_name << " " << fixed << setprecision(12) << value;
+        return oss.str();
+    }
+
+    void stamp(vector<vector<double>>& G, Circuit& circuit) override {
+        double conductance = 1.0 / value;
+        int idx1 = circuit.get_node_matrix_index(node1_name);
+        int idx2 = circuit.get_node_matrix_index(node2_name);
+
+        if (idx1 >= 0) G[idx1][idx1] += conductance;
+        if (idx2 >= 0) G[idx2][idx2] += conductance;
+        if (idx1 >= 0 && idx2 >= 0) {
+            G[idx1][idx2] -= conductance;
+            G[idx2][idx1] -= conductance;
+        }
+    }
 };
 
 int main() {
-    cout << "SUTSpice - Utility functions added." << endl;
-    try {
-        double val = parse_value_with_metric_prefix_util("1.5k");
-        cout << "Parsed '1.5k' to: " << val << endl;
-    } catch (const exception& e) {
-        cerr << "Error: " << e.what() << endl;
-    }
+    Circuit circuit;
+    circuit.add_component(make_unique<Resistor>("R1", "1", "0", "1k"));
+    cout << "SUTSpice - Resistor implemented with stamping." << endl;
+    cout << circuit.components[0]->to_netlist_string() << endl;
     return 0;
 }
